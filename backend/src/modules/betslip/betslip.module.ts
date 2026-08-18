7fbc8d06c0646a7f94dfd28e25eb60dcb7a93156
joeydd032995-pixel <joeydd032995@gmail.com>
2026-08-18 05:12:39 -0500

    feat(betslip): register automatic tracked settlement

--- backend/src/modules/betslip/betslip.module.ts
+++ backend/src/modules/betslip/betslip.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { BetslipController } from './betslip.controller';
 import { BetslipService } from './betslip.service';
 import { ClosingLineJob } from './closing-line.job';
+import { TrackedWagerSettlementJob } from './tracked-wager-settlement.job';
 import { WagerProjectionSnapshotService } from './wager-projection-snapshot.service';
 import { PrismaModule } from '../prisma/prisma.module';
 import { PlayerPropsModule } from '../player-props/player-props.module';
@@ -9,6 +10,11 @@ import { PlayerPropsModule } from '../player-props/player-props.module';
 @Module({
   imports: [PrismaModule, PlayerPropsModule],
   controllers: [BetslipController],
-  providers: [BetslipService, ClosingLineJob, WagerProjectionSnapshotService],
+  providers: [
+    BetslipService,
+    ClosingLineJob,
+    TrackedWagerSettlementJob,
+    WagerProjectionSnapshotService,
+  ],
 })
 export class BetslipModule {}
