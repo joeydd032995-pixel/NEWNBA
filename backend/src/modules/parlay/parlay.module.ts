62544c8b47d3abf1d1c74e4e59ec05177a0be92a
joeydd032995-pixel <joeydd032995@gmail.com>
2026-08-18 04:51:41 -0500

    feat(parlay): register empirical SGP engine

--- backend/src/modules/parlay/parlay.module.ts
+++ backend/src/modules/parlay/parlay.module.ts
@@ -1,13 +1,15 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from '../prisma/prisma.module';
 import { AnalyticsModule } from '../analytics/analytics.module';
+import { PlayerPropsModule } from '../player-props/player-props.module';
 import { ParlayService } from './parlay.service';
+import { EmpiricalSgpService } from './empirical-sgp.service';
 import { ParlayController } from './parlay.controller';
 
 @Module({
-  imports: [PrismaModule, AnalyticsModule],
-  providers: [ParlayService],
+  imports: [PrismaModule, AnalyticsModule, PlayerPropsModule],
+  providers: [ParlayService, EmpiricalSgpService],
   controllers: [ParlayController],
-  exports: [ParlayService],
+  exports: [ParlayService, EmpiricalSgpService],
 })
 export class ParlayModule {}
