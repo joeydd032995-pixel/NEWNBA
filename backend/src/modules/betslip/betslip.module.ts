a9e23391f7d67632413b65cc903c340b8a248a8e
joeydd032995-pixel <joeydd032995@gmail.com>
2026-08-18 02:52:08 -0500

    feat(tracking): register wager projection snapshot capture

--- backend/src/modules/betslip/betslip.module.ts
+++ backend/src/modules/betslip/betslip.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { BetslipController } from './betslip.controller';
 import { BetslipService } from './betslip.service';
 import { ClosingLineJob } from './closing-line.job';
+import { WagerProjectionSnapshotService } from './wager-projection-snapshot.service';
 import { PrismaModule } from '../prisma/prisma.module';
+import { PlayerPropsModule } from '../player-props/player-props.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, PlayerPropsModule],
   controllers: [BetslipController],
-  providers: [BetslipService, ClosingLineJob],
+  providers: [BetslipService, ClosingLineJob, WagerProjectionSnapshotService],
 })
 export class BetslipModule {}
