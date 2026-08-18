ca4b02a396892361e481fae6e7449bc9cf52d2fa
joeydd032995-pixel <joeydd032995@gmail.com>
2026-08-17 23:16:47 -0500

    feat: schedule expanded nba market ingestion

--- backend/src/services/background-jobs/jobs.module.ts
+++ backend/src/services/background-jobs/jobs.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ScheduleModule } from '@nestjs/schedule';
 import { JobsService } from './jobs.service';
 import { JobsController } from './jobs.controller';
+import { NbaMarketIngestionJob } from './nba-market-ingestion.job';
 import { EVModule } from '../../modules/ev/ev.module';
 import { ArbitrageModule } from '../../modules/arbitrage/arbitrage.module';
 import { PrismaModule } from '../../modules/prisma/prisma.module';
@@ -12,8 +13,18 @@ import { DataIngestionModule } from '../../modules/data-ingestion/data-ingestion
 import { NotificationsModule } from '../../modules/notifications/notifications.module';
 
 @Module({
-  imports: [ScheduleModule.forRoot(), EVModule, ArbitrageModule, PrismaModule, OddsApiModule, NbaDataModule, BallDontLieModule, DataIngestionModule, NotificationsModule],
+  imports: [
+    ScheduleModule.forRoot(),
+    EVModule,
+    ArbitrageModule,
+    PrismaModule,
+    OddsApiModule,
+    NbaDataModule,
+    BallDontLieModule,
+    DataIngestionModule,
+    NotificationsModule,
+  ],
   controllers: [JobsController],
-  providers: [JobsService],
+  providers: [JobsService, NbaMarketIngestionJob],
 })
 export class JobsModule {}
